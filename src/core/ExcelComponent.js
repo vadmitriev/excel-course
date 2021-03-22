bd791652ec95e7bbf8d6be40b4e5fd2d12697564
dmitriev.va <v.a.dmitriev123@gmail.com>
2021-03-22 20:45:12 +0500

    Finish base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,19 +4,41 @@ export class ExcelComponent extends DomListener {
     constructor($root, options = {}) {
         super($root, options.listeners)
         this.name = options.name || ''
+        this.emitter = options.emitter
+        this.unsubscribers = []
+
+        this.prepare()
     }
 
+    // Настраивает компонент до init
+    prepare() {}
 
     // возвращает шаблон компонента
     toHTML() {
         return ''
     }
 
+    // Уведомляем слушателей про событие event
+    $emit(event, ...args) {
+        this.emitter.emit(event, ...args)
+    }
+
+    // Подписываемся на событие event
+    $on(event, fn) {
+        const unsub = this.emitter.subscribe(event, fn)
+        this.unsubscribers.push(unsub)
+    }
+
+    // Инициализируем компонент
+    // Добавяет DOM слушателей
     init() {
         this.initDOMListeners()
     }
 
+    // Удаляет компонент
+    // Чистит слушателей
     destroy() {
         this.removeDOMListeners()
+        this.unsubscribers.forEach(unsub => unsub())
     }
 }
